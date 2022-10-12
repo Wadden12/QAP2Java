58474bba5c290f6f752bc94f09e9d10d9c1822c3
Mike Wadden <mike.wadden@keyin.com>
2022-10-12 16:39:33 -0230

    Finished Version1

--- src/main/java/com/qap2/exercise2/CreditCard.java
+++ src/main/java/com/qap2/exercise2/CreditCard.java
@@ -10,6 +10,8 @@ public class CreditCard {
     public CreditCard(Money limit, Person newCardHolder) {
         this.creditLimit = limit;
         this.owner = newCardHolder;
+        this.balance = new Money(0);
+
     }
 
     public Money getBalance() {
@@ -25,10 +27,22 @@ public class CreditCard {
     }
 
     public void charge(Money amount){
-        this.balance.add(amount);
+        System.out.println("Attempt to charge " + amount);
+        int balanceCheck = balance.add(amount).compareTo(getCreditLimit());
+
+        if(balanceCheck == 1){
+            System.out.println("Exceeds Credit Limit");
+            System.out.println("Balance: " + getBalance());
+            System.exit(0);
+        }
+
+     this.balance = getBalance().add(amount);
+        System.out.println("Charge:" + amount);
     }
 
     public void payment(Money amount){
-        this.balance.subtract(amount);
+        System.out.println("Attempt to pay " + amount);
+       this.balance= getBalance().subtract(amount);
+        System.out.println("Payment " + amount);
     }
 }
--- src/main/java/com/qap2/exercise2/CreditCardDemo.java
+++ src/main/java/com/qap2/exercise2/CreditCardDemo.java
@@ -7,16 +7,29 @@ public class CreditCardDemo {
         final Money LIMIT = new Money(1000);
         final Money FIRST_AMOUNT = new Money(200);
         final Money SECOND_AMOUNT =  new Money(10.02);
+        final Money TEST_AMOUNT = new Money(0.98);
         final Money THRID_Amount = new Money(25);
         final Money FOURTH_AMOUNT = new Money(990);
-        final Money TEST_AMOUNT = new Money(1200);
-
-        int compare = LIMIT.compareTo(TEST_AMOUNT);
-        System.out.println(compare);
-
         Person owner = new Person("Christie", "Diane", new Address("237J Harvey Hall", "Menomonie","WI","54751"));
-        System.out.println(owner);
-        Money money = new Money(1000.01);
-        System.out.println(money);
+        CreditCard visa = new CreditCard(LIMIT, owner);
+        System.out.println(visa.getPersonals());
+        printBalance(visa);
+        System.out.println("Credit Limit: " + visa.getCreditLimit());
+        System.out.println();
+        visa.charge(FIRST_AMOUNT);
+        printBalance(visa);
+        visa.charge(SECOND_AMOUNT);
+        printBalance(visa);
+        visa.charge(TEST_AMOUNT);
+        printBalance(visa);
+        visa.payment(THRID_Amount);
+        printBalance(visa);
+        visa.charge(FOURTH_AMOUNT);
+        printBalance(visa);
     }
+
+    private static  void printBalance(CreditCard money){
+        System.out.println("Balance: " + money.getBalance());
+    }
+
 }
